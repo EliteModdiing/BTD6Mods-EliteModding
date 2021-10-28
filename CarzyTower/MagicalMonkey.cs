db8d5f35570a7efef8e85f93033b9a39c4ccffe4
Elite Modding <93157370+EliteModdiing@users.noreply.github.com>
2021-10-28 08:05:20 -0500

    Update and rename CardMonkey.cs to MagicalMonkey.cs

--- CarzyTower/MagicalMonkey.cs
+++ CarzyTower/MagicalMonkey.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using Assets.Scripts.Models.Towers;
 using Assets.Scripts.Models.TowerSets;
@@ -18,25 +18,25 @@ namespace CardMonkey
         // public override string Portrait => "Don't need to override this, using the default of Name-Portrait";
         // public override string Icon => "Don't need to override this, using the default of Name-Icon";
 
-        public override string TowerSet => PRIMARY;
-        public override string BaseTower => TowerType.DartMonkey;
-        public override int Cost => 400;
+        public override string TowerSet => Magic;
+        public override string BaseTower => TowerType.Wizard;
+        public override int Cost => 1200;
 
         public override int TopPathUpgrades => 5;
         public override int MiddlePathUpgrades => 5;
         public override int BottomPathUpgrades => 5;
-        public override string Description => "Throws playing cards at Bloons";
+        public override string Description => "Uses magical powers to take down the bloons!!!";
 
-        // public override string DisplayName => "Don't need to override this, the default turns it into 'Card Monkey'"
+        public override string DisplayName => "Magical Monkey"
         public override void ModifyBaseTowerModel(TowerModel towerModel)
         {
-            towerModel.range += 10;
+            towerModel.range += 15;
             var attackModel = towerModel.GetAttackModel();
-            attackModel.range += 10;
+            attackModel.range += 15;
 
             var projectile = attackModel.weapons[0].projectile;
             projectile.ApplyDisplay<RedCardDisplay>(); // Make the projectiles look like Cards
-            projectile.pierce += 2;
+            projectile.pierce += 4;
         }
 
         /// <summary>
